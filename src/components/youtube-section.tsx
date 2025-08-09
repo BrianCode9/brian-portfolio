96d5f95f3883a7470f9a646cec0c85694e95e7f2
Brian Ngo <155392252+BrianCode9@users.noreply.github.com>
2025-08-09 17:19:00 -0400

    Vietlastic Layout is done

--- src/components/youtube-section.tsx
+++ src/components/youtube-section.tsx
@@ -4,18 +4,18 @@ const YouTubeSection = () => {
   return (
     <div className='bg-blue-600 p-20 flex justify-center'>
       <div className='flex flex-row gap-10'>
-        <div className='border flex flex-col border-2 p-20 w-150 gap-10'>
-          <div className='border-2 text-4xl'>Vietlastica</div>
-          <div className='border-2'>Description</div>
+        <div className='border flex flex-col border-2 p-10 w-150 gap-10'>
+          <div className='border-2 text-5xl'>Vietlastica Def put a cool font on this</div>
+          <div className='border-2'>This is my YouTube channel where I like posting videos about trips, coding projects, and school.</div>
         </div>
-        <div className='border-2 p-20 w-150 gap-20'>
-          <div className='flex grid grid-cols-2 grid-rows-3 gap-10 border-2'>
-            <div className='border-2'>hello</div>
-            <div className='border-2'>hello</div>
-            <div className='border-2'>hello</div>
-            <div className='border-2'>hello</div>
-            <div className='border-2'>hello</div>
-            <div className='border-2'>hello</div>
+        <div className='border-2 p-10 w-150'>
+          <div className='flex grid grid-cols-2 grid-rows-3 border-2'>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
+            <div className='border-2 p-5 w-full h-50 '>hello</div>
           </div>
         </div>
       </div>
