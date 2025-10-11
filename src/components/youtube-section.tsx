016c4bf7b7ddfbff60b2b101eba8e5a5e67a251e
Brian Ngo <155392252+BrianCode9@users.noreply.github.com>
2025-10-10 21:30:05 -0400

    Basic colors down for YT section

--- src/components/youtube-section.tsx
+++ src/components/youtube-section.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 
 const YouTubeSection = () => {
   return (
-    <div className='bg-white p-20 flex justify-center'>
-      <div className='flex flex-row gap-10 text-black'>
-        <div className='border flex flex-col border-2 p-10 w-150 gap-10'>
-          <div className='border-2 text-5xl'>Vietlastica</div>
-          Def put a cool font on this or put a background shape onto this
-          <div className='border-2 h-full'>This is my YouTube channel where I like posting videos about trips, coding projects, and school.</div>
-        </div>
-        <div className='border-2 p-10 w-150'>
-          <div className='flex grid grid-cols-2 grid-rows-3 border-2'>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
-            <div className='border-2 p-5 w-full h-50 '>hello</div>
+    <div className='bg-white flex justify-center'>
+      <div className='text-black w-600 flex justify-center bg-slate-300 pt-20 pb-20'>
+        <div className='flex flex-row gap-10'>
+          <div className='flex flex-col p-10 w-150 gap-10 text-black'>
+            <div className='text-6xl'>Vietlastica</div>
+            <div className='h-full'>This is my YouTube channel where I like posting videos about trips, personal projects, and educational videos.</div>
+          </div>
+          <div className='p-10 w-150 text-black'>
+            <div className='flex grid grid-cols-2 grid-rows-3 gap-2'>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+              <div className='p-5 w-full h-50 text-center border-2'>N/A</div>
+            </div>
           </div>
         </div>
       </div>
